ddad94fe74d5a2c1431093c268928788438d433e
LoGiCGOD <adutta786610@gmail.com>
2021-04-03 10:10:41 +0530

    directory and all_files functionalities are created

--- sort.py
+++ sort.py
@@ -5,8 +5,17 @@ folders = {
     'videos': ['.mp4'],
     'audios': ['.wav', '.mp3'],
     'images': ['.png', '.jpg'],
-    'documents': ['.doc', '.xlsx', '.xls', ]
-
-
+    'documents': ['.doc', '.xlsx', '.xls', '.pdf', '.zip', '.rar'],
 
 }
+print(folders)
+for fname in folders:
+    print(fname, folders[fname])
+
+
+directory = input("Enter the location or path:")
+# os.listdir() will take all the files and folders in the user given path and put it in a list
+
+all_files = os.listdir(directory)
+# all the files and folders are now in all_files
+print(all_files)
