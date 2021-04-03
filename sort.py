973dc7e194e8ac8aa72b9fa819398dda38a96b88
LoGiCGOD <adutta786610@gmail.com>
2021-04-03 09:01:12 +0530

    initial commit

---
+++ sort.py
@@ -0,0 +1,12 @@
+import os
+import shutil
+# os helps in handling folders while shutil helps in arranging files
+folders = {
+    'videos': ['.mp4'],
+    'audios': ['.wav', '.mp3'],
+    'images': ['.png', '.jpg'],
+    'documents': ['.doc', '.xlsx', '.xls', ]
+
+
+
+}
