be45623f87be172abcb8a60df34b9a62a4eef779
LoGiCGOD <adutta786610@gmail.com>
2021-04-03 17:18:54 +0530

    mov function created for creating folders

--- sort.py
+++ sort.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 # os helps in handling folders while shutil helps in arranging files
-folders = {
+folders = {                                  # Dictionary
     'videos': ['.mp4'],
     'audios': ['.wav', '.mp3'],
     'images': ['.png', '.jpg'],
@@ -10,7 +10,7 @@ folders = {
 }
 print(folders)
 for fname in folders:
-    print(fname, folders[fname])
+    print(fname, folders[fname])  # fname ~ foldername
 
 
 directory = input("Enter the location or path:")
@@ -19,3 +19,20 @@ directory = input("Enter the location or path:")
 all_files = os.listdir(directory)
 # all the files and folders are now in all_files
 print(all_files)
+
+
+def mov(extension, filename):  # def mov(extension, filename):
+    # print(extension, filename)
+    for fname in folders:
+        # try this to check the format and name of the file
+        if "."+extension in folders[fname]:
+            if fname not in os.listdir(directory):
+                os.mkdir(os.path.join(directory, fname))
+
+
+for i in all_files:
+    # isfile returns true if any files are present in the given path
+    if os.path.isfile(os.path.join(directory, i)) == True:
+        print("file exists")
+        # i.split will extract the extension while i will give the whole filename
+        mov(i.split(".")[-1], i)
